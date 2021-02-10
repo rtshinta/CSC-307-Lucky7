770d671b19d2cdcb5c0a0086fc71b7cbb8384de8
Ryan Shintaku <ryanshintaku@ryans-mbp.lan>
2021-02-09 16:44:16 -0800

    upload photo to flask api

--- backend/backend.py
+++ backend/backend.py
@@ -1,2 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, send_file
+from flask import request
+from flask import jsonify
+from flask_cors import CORS
+import random
+import string
+from db_mongodb import User
+
+
 app = Flask(__name__)
+CORS(app)
+#@app.route('/')
+#def welcome():
+#    return 'Welcome'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def upload_photo():
+    uploaded_photo = request.files['file']
+    if uploaded_photo.filename != '':
+        uploaded_photo.save(uploaded_photo.filename)
+        
+    return redirect(url_for('index'))
+
+@app.route('/get_image')
+def get_image():
+    # your code here
+    return send_file('testphoto.jpeg', mimetype='image/gif')
+
+
+
+
+#@app.route('/event/<id>', methods=['GET', 'POST'])
+#def get_or_post_event(id):
+
+
+
+
+#@app.route('/users', methods=['GET', 'POST'])
---
+++ backend/db_mongodb.py
@@ -0,0 +1,58 @@
+#mongodb+srv://admin:<password>@popup-307.ugbrh.mongodb.net/test
+#db_client = pymongo.MongoClient('mongodb+srv://csc307:csc307@popup-307.ugbrh.mongodb.net/<dbname>?retryWrites=true&w=majority')
+import pymongo
+from bson import ObjectId
+
+class Model(dict):
+    """
+    A simple model that wraps mongodb document
+    """
+    __getattr__ = dict.get
+    __delattr__ = dict.__delitem__
+    __setattr__ = dict.__setitem__
+
+    def save(self):
+        if not self._id:
+            self.collection.insert(self)
+        else:
+            self.collection.update(
+                { "_id": ObjectId(self._id) }, self)
+        self._id = str(self._id)
+
+    def reload(self):
+        if self._id:
+            result = self.collection.find_one({"_id": ObjectId(self._id)})
+            if result :
+                self.update(result)
+                self._id = str(self._id)
+                return True
+        return False
+
+    def remove(self):
+        if self._id:
+            resp = self.collection.remove({"_id": ObjectId(self._id)})
+            self.clear()
+            return resp
+
+class User(Model):
+    db_client = pymongo.MongoClient('mongodb+srv://csc307:csc307@popup-307.ugbrh.mongodb.net/<dbname>?retryWrites=true&w=majority')
+    collection = db_client["users"]["users_list"]
+
+    def find_all(self):
+        users = list(self.collection.find())
+        for user in users:
+            user["_id"] = str(user["_id"])
+        return users
+
+    def find_by_name(self, name):
+        users = list(self.collection.find({"name": name}))
+        for user in users:
+            user["_id"] = str(user["_id"])
+        return users
+
+    def find_by_name_and_job(self, name, job):
+        users = list(self.collection.find({"name": name, "job":job}))
+        for user in users:
+            user["_id"] = str(user["_id"])
+        return users
+    
