c9d9e9306155c1bcc473201eadd4a6c5c9475801
Ryan Shintaku <ryanshintaku@ryans-mbp.lan>
2021-02-22 14:25:35 -0800

    add test cases to backend and frontend

--- flask/sample_backend_tests.py
+++ flask/sample_backend_tests.py
@@ -7,10 +7,21 @@ url = 'http://127.0.0.1:5000/'
 
 def test_hello():
     response = app.test_client().get('/')
-
     assert response.status_code == 200
     assert response.data == b'Hello, World! And Travis'
 
 def test_getUsers():
     response = app.test_client().get('/users')
     assert response.status_code == 200
+
+def test_postUsers():
+    response = app.test_client().post('/users', json={
+      "date": "5/5/5555", 
+      "description": "dfba", 
+      "event": "gdsf", 
+      "location": "dfgb", 
+      "photo": "https://iso.500px.com/wp-content/uploads/2016/02/stock-photo-133673159-1500x1000.jpg", 
+      "rating": "5", 
+      "tags": "onefish, twofish, redfish, bluefish"
+    })
+    assert response.status_code == 201
--- src/helper.test.js
+++ src/helper.test.js
@@ -1,3 +1,44 @@
-test('2 + 2 should return 4', () => {
-    expect(2 + 3).toBe(5);
-  });
+import CardBody from './CardBody'
+
+test('2 + 2 should return 4', () => 
+{
+  expect(2 + 2).toBe(4);
+});
+
+test('tests description field of cardbody', () => 
+{
+  let character = {
+    "_id": {
+        "$oid": "60233179aba6d37fafe9fae1"
+    },
+    "photo": "https://i.imgur.com/wBk6SwF.jpg",
+    "description": "Burger Consumer",
+    "event": "Blood Man",
+    "date": "2/12/2021",
+    "location": "San Luis Obispo, CA",
+    "tags": "food truck",
+    "rating": "1"
+};
+  const wrapper = (<CardBody characterData={character} removeCharacter={null} />);
+  const value = wrapper.props.characterData.description
+  expect(value).toMatch(/Burger Consumer/);
+});
+
+test('tests date field of cardbody component', () => 
+{
+  let character = {
+    "_id": {
+        "$oid": "60233179aba6d37fafe9fae1"
+    },
+    "photo": "https://i.imgur.com/wBk6SwF.jpg",
+    "description": "Burger Consumer",
+    "event": "Blood Man",
+    "date": "2/12/2021",
+    "location": "San Luis Obispo, CA",
+    "tags": "food truck",
+    "rating": "1"
+};
+  const wrapper = (<CardBody characterData={character} removeCharacter={null} />);
+  const value = wrapper.props.characterData.date
+  expect(value).toMatch("2/12/2021");
+});
