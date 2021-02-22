c2a504d6665e39b3fd98225b6360c441443b2b34
Ryan Shintaku <ryanshintaku@ryans-mbp.lan>
2021-02-22 10:53:19 -0800

    add test cases to backend

--- flask/sample_backend_tests.py
+++ flask/sample_backend_tests.py
@@ -1,5 +1,8 @@
 import pytest
 import sample_backend
+import requests
+
+url = 'http://127.0.0.1:5000/'
 
 # example tests
 def test_find_users_by_name_success():  
@@ -20,3 +23,12 @@ def test_find_users_by_name_success():
 def test_find_users_by_name_fail():  
     expected = [] 
     assert sample_backend.find_users_by_name("Jeff") == expected
+
+def test_homepage():
+    r = requests.get(url)
+    assert r.text == 'Hello, World! And Travis'
+    assert r.status_code == 200
+
+def test_getUsers():
+    r = requests.get(url + 'users')
+    assert r.status_code == 200
