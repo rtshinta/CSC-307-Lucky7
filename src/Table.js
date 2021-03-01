6f1fbbe6d6b4bb39381c78cf774b99796c2b8cfc
Jonathan Fisher <jpfisher72@gmail.com>
2021-03-01 10:27:43 -0800

    Deleted some unused code. Not exhaustive

--- src/Table.js
+++ src/Table.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react'
 import CardBody from './CardBody'
 
-const TableHeader = () => {
-  return (
-    <thead>
-      {/* <NavBar></NavBar> */}
-      {/* No longer calling NavBar here, instead calling in App's render() */}
-    </thead>
-  )
-}
-
 const Table = props => {
   const { characterData, removeCharacter } = props
   
   return (
     <table>
-      {/* <TableHeader /> */}
       <ul>
         <CardBody characterData={characterData} removeCharacter={removeCharacter} />
       </ul>
