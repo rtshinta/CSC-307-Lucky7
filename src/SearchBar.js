8dadf026852b7b5182caf5b2fd85d0ccf93d9636
Jonathan Fisher <jpfisher72@gmail.com>
2021-03-06 12:11:15 -0800

    Improved interacvtivity cues and color matching to prototype

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -22,34 +22,53 @@ class SearchBar extends Component
     })
   }
 
+
+  //Upon button click, update shown result.
+  submitForm = () => {
+    //this.setState(this.state);
+    this.setState({
+      ['searchEnter']: this.state.currentInput,
+    })
+  }
+
+
   render() {
-    const { currentInput } = this.state;
+    const { currentInput, searchEnter } = this.state;
     const { searchFunction } = this.props;
 
+    /*return (
+      <form>
+        <label htmlFor='currentInput'></label>
+        <input type="text" name="currentInput" id="currentInput" value={currentInput} onChange={this.searchSubmit} />
+        <input type="button" value="Submit" onClick={() => this.props.searchFunction(currentInput)} />
+        <p>{searchEnter}</p>
+      </form>
+    );*/
 
     return(
-      <nav className="navbar navbar-default"style={{padding:'0px',backgroundColor:'transparent',borderColor:'transparent'}}>
-          <div className="container-fluid" style = {{padding:'0px'}}>
-              <div className="navbar-header">
-                  <a className="navbar-brand" href="http://localhost:3000/">
-                      <img src= "logo_icon.png" alt = "Failed" style={{display:'flex', justifyContent:'center',alignItems:'center',marginTop:'1px',height:"50px"}}/>
+      <nav class="navbar navbar-default"style={{padding:'0px',backgroundColor:'transparent',borderColor:'transparent'}}>
+          <div class="container-fluid" style = {{padding:'0px'}}>
+              <div class="navbar-header">
+                  <a class="navbar-brand" href="http://localhost:3000/">
+                      <img src= "logo_icon.png" style={{display:'flex', justifyContent:'center',alignItems:'center',marginTop:'1px',height:"50px"}}/>
                   </a>
               </div>
-              <form className="navbar-form navbar-right">
+              <form class="navbar-form navbar-right">
                 <label htmlFor='currentInput'></label>
-                <div className="form-group">
-                  <input type="text" name="currentInput" id="currentInput" className = "form-control" 
+                <div class="form-group">
+                  <input type="text" name="currentInput" id="currentInput" class = "form-control" 
                   value={currentInput} onChange={this.searchSubmit} placeholder="Search" style ={{width: '500px'}}/>
                 </div>
-                <button type="button" id="srchBtn" aria-label = "srchBtn" onClick={() => searchFunction(currentInput)}
-                style={{borderRadius:50,height:'40px',width:'40px',/*marginTop:'20px',*/marginLeft: '5px'}}>
+                <button type="button" onClick={() => this.props.searchFunction(currentInput)}
+                style={{borderRadius:50,height:'40px',width:'40px',marginLeft: '5px'}}>
                   <i class="glyphicon glyphicon-search" style={{display:'flex', justifyContent:'center',alignItems:'center',fontSize:'17px'}}></i>
                 </button>
+                <p>{searchEnter}</p>
               </form>
-              <div className="btn-group">
-                  <button type="button" className="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+              <div class="btn-group">
+                  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
                   style={{borderRadius:50,height:'40px',width:'40px',marginTop:'15px'}}>
-                      <i className="glyphicon glyphicon-menu-hamburger" style={{display:'flex', justifyContent:'center',alignItems:'center',fontSize:'17px'}}></i>
+                      <i class="glyphicon glyphicon-menu-hamburger" style={{display:'flex', justifyContent:'center',alignItems:'center',fontSize:'17px'}}></i>
                   </button>
                   <div className="dropdown-menu">
                       <a className="dropdown-item" href="/">Home</a>
@@ -57,9 +76,6 @@ class SearchBar extends Component
                       <a className="dropdown-item" href="/eventform">Form</a>
                           <div className="dropdown-divider"></div>
                       <a className="dropdown-item" href="/about">About</a>
-                          <div className="dropdown-divider"></div>
-                      <a className="dropdown-item" href="/eventdetails">EventDetails</a>
-
                   </div>
               </div>
           </div>
