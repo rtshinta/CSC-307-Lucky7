833541dd95a271ee3288e607be340ef94b84f46e
Coby Hong <cobyhong@gmail.com>
2021-03-09 15:23:22 -0800

    Push card fix to display image when have multiple images uploaded

--- src/CardBody.js
+++ src/CardBody.js
@@ -66,7 +66,7 @@ const CardBody = props => {
           }}  onClick={() => props.setInfo(row)}>
 
                 <Card style={{ boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)', width: '300px', height: "400px", borderRadius: '2rem',}}>
-                  <Card.Img style={{ height: '30%', objectFit: 'cover', borderTopLeftRadius: '2rem', borderTopRightRadius: '2rem'}} src={row.photo} />
+                  <Card.Img style={{ height: '30%', objectFit: 'cover', borderTopLeftRadius: '2rem', borderTopRightRadius: '2rem'}} src={row.photo.split(", ")[0]} />
                   <div style={{ textAlign: 'center', fontSize: '1.5rem', letterSpacing: '-1px', height: '2.5rem', color: 'white', backgroundColor: 'rgb(17, 134, 242)'}}>{d}</div>
                   <Card.Body>
                         <Card.Title>{trimmed_name}</Card.Title>
