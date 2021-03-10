b06bec800a20ae75f02d71ab8bf8fc788d13b559
Jonathan Fisher <jpfisher72@gmail.com>
2021-03-09 20:43:50 -0800

    Lil CSS cleanup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-//Note: style.css is empty but project won't build without it? Should fix
+//Note: style.css is empty but project won't build without it
 import './style.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
